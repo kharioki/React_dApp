07bf5b990e720ab244f3839ba3dcec31c266070a
tony <antolisah@gmail.com>
2021-10-20 19:21:06 +0300

    infura config with ropsten url

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+const KEY = process.env.REACT_APP_PRIVATE_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,13 +20,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
   paths: {
     artifacts: './src/artifacts',
   },
   networks: {
-    hardhat: {
-      chainId: 1337,
+    hardhat: {},
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/928420a535e2427695b9692b7be44e39",
+      accounts: [`0x${KEY}`]
     },
   },
+  solidity: "0.8.4",
 };
